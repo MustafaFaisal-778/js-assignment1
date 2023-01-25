1c08c332cddbe6264a7604f4b498ab00a4e71fcd
Mustafa Faisal <122450274+MustafaFaisal-778@users.noreply.github.com>
2023-01-25 14:37:34 +0500

    Add files via upload

---
+++ JS Alert/app.js
@@ -0,0 +1,16 @@
+// Question#1
+// alert("Hello World")
+
+// Question#2
+// alert("Error! Please enter a valid password.")
+
+// Question#3
+// alert("Welcome to JS land...\n Happy Coding!")
+
+// Question#4
+// var display="Welcome to JS land"
+// alert(display)
+// alert("Happy Coding")
+
+// Question#5
+// alert("Hello...I can run js through my web browser's console")
